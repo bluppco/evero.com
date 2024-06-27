4b7fa88a168cf8d649a6380522789c652ac07be5
Shiva pandey <shivapandey9898@gmail.com>
2024-06-27 14:55:15 +0530

    feat
    added section footer

--- src/content/config.js
+++ src/content/config.js
@@ -22,8 +22,29 @@ const headerCollection = defineCollection({
 
 })
 
+const footerCollection = defineCollection({
+
+	schema: z.object({
+
+		title: z.string(),
+		order: z.number(),
+		items: z.array(
+
+			z.object({
+				title: z.string(),
+				link: z.string(),
+				order: z.number(),
+			})
+
+		).optional(),
+
+	})
+
+})
+
 export const collections = {
 
-	"header": headerCollection
+	"header": headerCollection,
+	"footer": footerCollection
 
 }
