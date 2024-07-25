3e571b59dcdd0e4bf2bd9fae504b898c0f1328eb
Tanish Sharma <tanishsharmaab17@gmail.com>
2024-07-25 07:32:03 +0530

    feat: add API in home page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,37 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
-import cloudflare from "@astrojs/cloudflare";
-import mdx from "@astrojs/mdx";
+import cloudflare from "@astrojs/cloudflare"
+import mdx from "@astrojs/mdx"
+import react from "@astrojs/react"
+import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), mdx()],
-  output: "server",
-  adapter: cloudflare()
-});
+
+	integrations: [tailwind(), react(), mdx()],
+	output: "server",
+	adapter: cloudflare({
+
+		routes: {
+
+			exclude: ["/icons/*", "/fonts/*", "/images/*", "/logo/*"]
+
+		},
+		runtime: {
+
+			mode: "local",
+			type: "pages",
+
+		}
+
+	}),
+	image: {
+
+		service: {
+
+			entrypoint: "astro/assets/services/noop"
+
+		}
+	},
+
+})
